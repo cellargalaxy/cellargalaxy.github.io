d5fa4358ecacfa496bd5e954a07f8747e574a582
cellargalaxy <cellargalaxy@gmail.com>
2022-02-04 16:09:14 +0800

    1

---
+++ _nuxt/static/1643962128/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","view\u002FREADME","view\u002F折腾\u002Fn1安装armbian做小pc","\u002Ftest","page\u002F1","page\u002F折腾\u002F1","view\u002F折腾\u002F博客评论系统","view\u002F折腾\u002Fgrafana+loki+promtail搭建轻量日志系统","view\u002F折腾\u002F安装ubuntu18常用软件手记","view\u002F折腾\u002Fvps重装系统的习惯性操作","view\u002F折腾\u002F小米10S刷机"]})
---
+++ _nuxt/static/1643962128/view/折腾/博客评论系统/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1643962128",layout:"default",error:a,serverRendered:true,routePath:"\u002Fview\u002F%E6%8A%98%E8%85%BE\u002F%E5%8D%9A%E5%AE%A2%E8%AF%84%E8%AE%BA%E7%B3%BB%E7%BB%9F",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"2ae1c681"}}}}(null));
